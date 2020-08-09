65bac3da95f0748331e3627063397f08e1ecc772
Sushant Sahu <sushant87@gmail.com>
2020-08-09 17:41:02 +0530

    kube-101 init

---
+++ kube-101/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+let users = require("./data");
+let unique_id = 1;
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/users", (req, resp) => {
+  resp.send({ users });
+});
+
+app.post("/user", (req, resp) => {
+  const {
+    user: { id, firstname, lastname, gender },
+  } = req.body;
+  const usertoupdate = users.find((item) => item.id === id);
+  const updateduser = { ...usertoupdate, firstname, lastname, gender };
+  const newusers = users.filter((item) => item.id != id);
+  users = [...newusers, updateduser];
+  resp.send({  user: updateduser });
+});
+
+app.put("/user", (req, resp) => {
+  const { user } = req.body;
+  console.log(user);
+  user.id = unique_id++;
+  users.push(user);
+  resp.send({ user_id:user.id });
+});
+
+app.delete("/user/:userid", (req, resp) => {
+  const { userid } = req.params;
+  const newusers = users.filter((item) => item.id != userid);
+  users = newusers;
+  resp.send({ id:userid });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
